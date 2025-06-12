37829d8345cd345485a87f2503095b4cc6473479
kiran kiran <kiranyadavsde00121@gmail.com>
2025-06-13 01:48:01 +0530

    new project

---
+++ script.js
@@ -0,0 +1,67 @@
+console.log("lets write javascript");
+let currentSong = new Audio();
+
+async function getsongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+
+    }
+    return songs
+
+}
+const playMusic = (track) => {
+    // let audio=new Audio("/songs/"+track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+}
+async function main() {
+
+    let songs = await getsongs()
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("UL")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>kiran</div>
+                            </div>
+                            <div class="playNow">
+                                <span>Play Now</span>
+                                <img class="invert" src="paly.svg" alt="playNow">
+
+                            </div></li>`;
+    }
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+
+    });
+    const play=document.querySelector("#play")
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "paly.svg"
+        }
+    })
+}
+
+
+main()
+
